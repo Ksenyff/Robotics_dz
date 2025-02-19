fb2a89ff10d2e6bfa36aaea7114fb44175d76d5c
Ksenyff <149685810+Ksenyff@users.noreply.github.com>
2025-02-19 11:57:20 +0300

    dz4_2.m

--- dz4_2.m
+++ dz4_2.m
@@ -28,7 +28,7 @@ len = 10;
  figure 
 hold on;
 plot3(mas(:,1),mas(:,2),mas(:,3),'k--', 'LineWidth', 1.5)
-for i = 1%length(TH)
+for i = 1:length(TH)
     T = TH{i}; % Извлекаем матрицу преобразования
     
     % Позиция начала координат
@@ -51,38 +51,6 @@ for i = 1%length(TH)
     
     % Ось Z (синяя)
     quiver3(x0, y0, z0, zAxis(1)*len, zAxis(2)*len, zAxis(3)*len, 'b', 'LineWidth', 2);
-    text(x0 + zAxis(1)*len, y0 + zAxis(2)*len, z0 + zAxis(3)*len, 'Z', 'Color', 'b');
-    
-    % Подпись для каждого звена
-    text(x0, y0, z0 - len, ['Joint ', num2str(i)], 'Color', 'k', 'FontSize', 10);
-    
-end
-
-hold on;
-plot3(mas(:,1),mas(:,2),mas(:,3),'k--', 'LineWidth', 1.5)
-for i = 2%length(TH)
-    T = TH{i}; % Извлекаем матрицу преобразования
-    
-    % Позиция начала координат
-    x0 = T(1, 4);
-    y0 = T(2, 4);
-    z0 = T(3, 4);
-    
-    % Направления осей
-    xAxis = T(1:3, 1); % Направление оси X
-    yAxis = T(1:3, 2); % Направление оси Y
-    zAxis = T(1:3, 3); % Направление оси Z
-       
-    % Ось X (красная)
-    quiver3(x0, y0, z0, xAxis(1)*len, xAxis(2)*len, xAxis(3)*len, 'r', 'LineWidth', 5);
-    text(x0 + xAxis(1)*len, y0 + xAxis(2)*len, z0 + xAxis(3)*len, 'X', 'Color', 'r');
-    
-    % Ось Y (зеленая)
-    quiver3(x0, y0, z0, yAxis(1)*len, yAxis(2)*len, yAxis(3)*len, 'g', 'LineWidth', 5);
-    text(x0 + yAxis(1)*len, y0 + yAxis(2)*len, z0 + yAxis(3)*len, 'Y', 'Color', 'g');
-    
-    % Ось Z (синяя)
-    quiver3(x0, y0, z0, zAxis(1)*len, zAxis(2)*len, zAxis(3)*len, 'b', 'LineWidth', 5);
     text(x0 + zAxis(1)*len, y0 + zAxis(2)*len, z0 + zAxis(3)*len, 'Z', 'Color', 'b');
     
     % Подпись для каждого звена
